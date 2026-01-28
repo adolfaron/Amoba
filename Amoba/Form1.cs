1c984d84e3dc9e1e1795424ecdc1caad9ae7ad95
alexmester <150438883+alexmester@users.noreply.github.com>
2026-01-28 09:42:13 +0100

    rács létrehozása

--- Amoba/Form1.cs
+++ Amoba/Form1.cs
@@ -2,9 +2,20 @@ namespace Amoba
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            jatekter1 ujjatek=new jatekter1();
+            ujjatek.ShowDialog(); try
+            {
+                this.Show();
+            }
+            catch (Exception) { Application.Exit(); };
         }
     }
 }
---
+++ Amoba/jatekter1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Amoba
+{
+
+    public partial class jatekter1 : Form
+    {
+        const int meret = 30;
+        PictureBox[,] cellak = new PictureBox[meret, meret];
+        public jatekter1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+            Text = "Amóba";
+            for (int sor = 0; sor < meret; sor++)
+            {
+                for (int oszlop = 0; oszlop < meret; oszlop++)
+                {
+                    cellak[sor, oszlop] = new PictureBox();
+                    PictureBox cella =cellak[sor, oszlop];
+                    cella.Size = new Size(20, 20);
+                    cella.BackColor = Color.LightGray;
+                    cella.Location = new Point(oszlop * (cella.Size.Width + 3) + 12, sor * (cella.Size.Height + 3) + 12);
+                        this.Controls.Add(cella);
+                }
+            }
+        }
+    }
+}

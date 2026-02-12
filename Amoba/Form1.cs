185e2efb186373ebc8f7f1134714298fbaffbbd7
adolfaron <adolfaron@turr.hu>
2026-02-12 21:42:34 +0100

    maxlerak beállít

--- Amoba/Form1.cs
+++ Amoba/Form1.cs
@@ -209,11 +209,6 @@
                 lerakotmax.Visible = false;
             }
         }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
 
